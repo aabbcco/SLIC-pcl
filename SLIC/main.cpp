285f88b149c0d43ee84ade192e266a42fbc33753
aabbcco <fooly_big_gun@163.com>
2020-04-10 17:20:07 +0900

    完成主要框架，算法到能用水平

--- SLIC/main.cpp
+++ SLIC/main.cpp
@@ -38,7 +38,8 @@ int main()
 {	
 	float s = 0.2f;
 	float m = 1.0f;
-	int samples = 65;
+	int samples = 200;
+	float L2_min = 0.05f;
 /*
 	for (int i = 0; i < 13; i++)
 	{
@@ -74,38 +75,46 @@ int main()
 	kd_tree.setInputCloud(cloud);
 	std::vector<int>search_indices;
 	std::vector<float>point_square_dist;
-	float x, y, z;
-
-	for (int i = clusteing_center->width-1; i >= 0; i--)//each seed point
+	float x, y, z,L2;
+	do
 	{
-		search_indices.clear();
-		point_square_dist.clear();
-		kd_tree.radiusSearch((*clusteing_center)[i], 2*s, search_indices, point_square_dist, 0);
-		x = y = z = 0; //clear x,y,z
-		for (int j = search_indices.size() - 1; j >= 0; j--)//each point in 2S radius
+		copyPointCloud(*new_clusteing_center, *clusteing_center);
+		for (int i = clusteing_center->width - 1; i >= 0; i--)//each seed point
 		{
-			float dist_color = 0.0f;
-			float dist_slic = std::sqrt(dist_color + m * m*point_square_dist[j] / (s*s));
-			if (dist_slic < slic_dist[search_indices[j]])
+			search_indices.clear();
+			point_square_dist.clear();
+			kd_tree.radiusSearch((*clusteing_center)[i], 2 * s, search_indices, point_square_dist, 0);
+			x = y = z = 0; //clear x,y,z
+			float count = 0.0f;
+			for (int j = search_indices.size() - 1; j >= 0; j--)//each point in 2S radius
 			{
-				slic_dist[search_indices[j]] = dist_slic;
-				lable[search_indices[j]] = i;
-				x += (*cloud)[search_indices[j]].x;
-				y += (*cloud)[search_indices[j]].y;
-				z += (*cloud)[search_indices[j]].z;
+				float dist_color = 0.0f;
+				float dist_slic = std::sqrt(dist_color + m * m*point_square_dist[j] / (s*s));
+				if (dist_slic < slic_dist[search_indices[j]])
+				{
+					slic_dist[search_indices[j]] = dist_slic;
+					lable[search_indices[j]] = i;
+					x += (*cloud)[search_indices[j]].x;
+					y += (*cloud)[search_indices[j]].y;
+					z += (*cloud)[search_indices[j]].z;
+					count += 1.0f;
+				}
 			}
 			//compute new seed
-			(*new_clusteing_center)[i].x = x / search_indices.size();
-			(*new_clusteing_center)[i].y = y / search_indices.size();
-			(*new_clusteing_center)[i].z = z / search_indices.size();
-
-			//compute L2 norm
-			for (int i = clusteing_center->width - 1; i >= 0; --i)
-			{
-
-			}
+			(*new_clusteing_center)[i].x = x / count;
+			(*new_clusteing_center)[i].y = y / count;
+			(*new_clusteing_center)[i].z = z / count;
 		}
-	}
+		//compute L2 norm
+		L2 = 0.0f;
+		for (int i = clusteing_center->width - 1; i >= 0; --i)
+		{
+			L2 +=	((*new_clusteing_center)[i].x - (*clusteing_center)[i].x)*((*new_clusteing_center)[i].x - (*clusteing_center)[i].x)
+				+	((*new_clusteing_center)[i].y - (*clusteing_center)[i].y)*((*new_clusteing_center)[i].y - (*clusteing_center)[i].y)
+				+	((*new_clusteing_center)[i].z - (*clusteing_center)[i].z)*((*new_clusteing_center)[i].z - (*clusteing_center)[i].z);
+		}
+		std::cout << "literation done L2= "<<L2 << endl;
+	} while (L2 >= L2_min);
 
 	PointCloud<PointTC>::Ptr labledcloud(new PointCloud<PointTC>);
 	copyPointCloud(*cloud, *labledcloud);
